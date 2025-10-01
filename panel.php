a5536c750cdf8c95143621b650052d5765977f3d
TheSzlamek <127144748+TheSzlamek@users.noreply.github.com>
2025-10-01 20:11:37 +0200

    panel.php

--- panel.php
+++ panel.php
@@ -1,24 +1,12 @@
 <?php
 session_start();
-if (empty($_SESSION['logged_in'])) {
-    header("Location: login.php?msg=" . urlencode("Najpierw się zaloguj"));
+if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+    header("Location: login.html");
     exit;
 }
 ?>
-<!doctype html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Panel</title>
-</head>
-<body>
-<h2>Panel użytkownika</h2>
-<p>Witaj! To jest Twój panel.</p>
-<p>
-    <a href="change_password.php">Zmień hasło</a> |
-    <a href="logout.php">Wyloguj</a>
-</p>
-</body>
-</html>
+<h1>Witaj w panelu!</h1>
+<a href="logout.php">Wyloguj</a>
+
 
 
